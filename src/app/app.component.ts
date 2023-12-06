7b9d51f9010900b8827754e49745395ecf71452c
abe <abdulah.21014066@stud.uoz.edu.krd>
2023-12-06 08:29:03 -0800

    form

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'HW';
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Form Submitted!', form.value);
+    } else {
+      console.log('Form is invalid!');
+    }
+  }
+}
